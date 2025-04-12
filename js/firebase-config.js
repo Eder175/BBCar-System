af2bccce0d764e0e766b91a97dc8116e5630f514
Eder <souzaeder175@gmail.com>
2025-04-12 05:14:24 +0100

    Adicionar js/firebase-config.js ao projeto

---
+++ js/firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "sua-api-key",
+    authDomain: "seu-projeto.firebaseapp.com",
+    projectId: "seu-projeto",
+    storageBucket: "seu-projeto.appspot.com",
+    messagingSenderId: "seu-sender-id",
+    appId: "seu-app-id"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
