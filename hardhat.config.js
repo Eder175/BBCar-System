2c7fc192859d29510523fb44479e71dee9ddbc50
Eder <souzaeder175@gmail.com>
2025-04-12 04:45:28 +0100

    Atualizar .env com chave privada e configurar Amoy Testnet no hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,11 +2,12 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
 module.exports = {
-    solidity: "0.8.0",
-    networks: {
-        mumbai: {
-            url: "https://rpc-mumbai.maticvigil.com",
-            accounts: [process.env.PRIVATE_KEY],
-        },
+  solidity: "0.8.24",
+  networks: {
+    amoy: {
+      url: "https://rpc-amoy.polygon.technology",
+      chainId: 80002,
+      accounts: [process.env.PRIVATE_KEY],
     },
+  },
 };
