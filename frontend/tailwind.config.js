f0e7c264543b8865ab20e5a707d4601833cc8ca6
Eder <souzaeder175@gmail.com>
2025-04-29 16:46:07 +0100

    Corrige Tailwind CSS, adiciona about.html e ajusta index.html

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html", "./src/**/*.{html,js}"],
+  content: [
+    "./index.html",           // Escaneia o index.html na raiz do frontend
+    "./pages/**/*.{html,js}", // Escaneia todos os arquivos HTML e JS na pasta pages/
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
